b60a20d8db58e18eb279d8394aafc19d87348671
Drazen Drinic <drazendrinic@hotmail.com>
2023-04-29 23:44:56 +0200

    add: Firebase initial setup and button and input style

--- index.js
+++ index.js
@@ -1,7 +1,38 @@
+/* FireBase Stuff & Browser module stuff*/
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.21.0/firebase-app.js'
+import { getDatabase, ref, push } from 'https://www.gstatic.com/firebasejs/9.21.0/firebase-database.js'
+
+
+
+/* My database */
+const firebaseConfig = {
+    databaseURL: "https://todo-c9874-default-rtdb.europe-west1.firebasedatabase.app/"
+}
+
+
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig)
+
+// Initialize Realtime Database and get a reference to the service
+const database = getDatabase(app)
+
+// Referencing the database and creating a table in it
+const toDoDatabase = ref(database, "toDoDatabase")
+
+
+/* App Stuff */ 
+
 const inputFieldEl = document.getElementById("input-field")
 const toDoButtonEl = document.getElementById("add-button")
 
 toDoButtonEl.addEventListener("click", function(){
     let inputValue = inputFieldEl.value
-    console.log(inputValue)
+    
+    // Push input value to the db
+    push(toDoDatabase, inputValue)
+    console.log(inputValue + " added to the Firebase")
+
+    // Clear input field
+    inputFieldEl.value = ""
 })
