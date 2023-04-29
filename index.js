69e28d8247d6aada8dcb35ddc9dc392b60441057
Drazen Drinic <drazendrinic@hotmail.com>
2023-04-29 22:45:52 +0200

    style: arrange and color the App

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// JS
+const inputFieldEl = document.getElementById("input-field")
+const toDoButtonEl = document.getElementById("add-button")
+
+toDoButtonEl.addEventListener("click", function(){
+    let inputValue = inputFieldEl.value
+    console.log(inputValue)
+})
